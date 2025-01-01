751efcb9326b5ab7cfebf5c400e88411936255d2
Maryna Haiduk <maryna.i.haiduk@gmail.com>
2025-01-01 18:12:31 -0500

    Added Recipe model

---
+++ FoodPlannerApp/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FoodPlannerApp.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; } // Primary Key
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; } // URL or File Path
+        public List<string> Ingredients { get; set; } = new List<string>();
+        public string Instructions { get; set; }
+        public string Type { get; set; } // Example: "Breakfast", "Lunch", etc.
+    }
+}
